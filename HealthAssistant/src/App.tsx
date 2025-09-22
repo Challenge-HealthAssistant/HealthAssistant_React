1004242b1c05c1bf2cc4034b624110f9a8ec8169
arthurscamara <arthurcamarakai@gmail.com>
2025-09-22 14:00:13 -0300

    criei a pasta routes e folders e files das páginas do projeto

--- HealthAssistant/src/App.tsx
+++ HealthAssistant/src/App.tsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Footer } from "./components/Footer/footer";
+import { Header } from "./components/Header/header";
 
 function App() {
   
