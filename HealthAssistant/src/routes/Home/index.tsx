aa9e9694c7f9d06c970dd3bbac48ab3aaaf91f24
PietroWilhelm <pietrowilhelm@git.com.br>
2025-09-23 15:42:11 -0300

    pasta img criada com as imagens do sistema

--- HealthAssistant/src/routes/Home/index.tsx
+++ HealthAssistant/src/routes/Home/index.tsx
@@ -5,10 +5,6 @@ export default function Home() {
   
   return (
     <div className="bg-[#2196c9] min-h-screen flex flex-col">
-      <div className="bg-white px-4 py-4 flex items-center justify-between">
-        <img src="/logo.png" alt="Logo" className="h-12" />
-        <span className="text-4xl">👤</span>
-      </div>
       <div className="flex-1 flex flex-col justify-center items-center gap-6">
         <Link className=" w-56 py-4 rounded-lg border-none bg-[#eaeaea] text-[#1976a5] font-semibold text-lg" to="/teleconsulta">
           Teleconsulta
--- HealthAssistant/src/routes/Perfil/index.tsx
+++ HealthAssistant/src/routes/Perfil/index.tsx
@@ -1,7 +1,11 @@
+import Links from "../../components/Links/Links";
+import Menu from "../../components/Menu/Menu";
+
 export default function Perfil () {
     return (
         <nav>
-
+            <Menu />
+            <Links />
         </nav>
     );
 }
