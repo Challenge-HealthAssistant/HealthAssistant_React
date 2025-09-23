3b69bdfa31784eaaefac85c962f3dd3f7fa3a32b
PietroWilhelm <pietrowilhelm@git.com.br>
2025-09-23 15:41:13 -0300

    adicao do menu e links para pagina suporte apenas para a navegacao enquanto a pagina não está feita

--- HealthAssistant/src/routes/Suporte/index.tsx
+++ HealthAssistant/src/routes/Suporte/index.tsx
@@ -1,10 +1,13 @@
 import Links from "../../components/Links/Links";
+import Menu from "../../components/Menu/Menu";
 
 
 export default function Suporte() {
     return (
         <nav>
+            <Menu />
             <Links />
+        
         </nav>
     );
 }
