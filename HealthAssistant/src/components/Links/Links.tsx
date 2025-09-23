b6a4934d706d18e2c4b9986763d9de01534f5e59
PietroWilhelm <pietrowilhelm@git.com>
2025-09-23 18:51:38 -0300

    adicao das imagens dos bottom links e css foi jogado para global.css

--- HealthAssistant/src/components/Links/Links.tsx
+++ HealthAssistant/src/components/Links/Links.tsx
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
+import home from "../../img/home.png";
+import quemSomos from "../../img/quem_somos.png";
+import faq from "../../img/faq.png";
+import chatbot from "../../img/chatbot.png";
 
 export default function Links() {
     return (
-        <nav className="flex justify-around bg-[#1de9b6] py-3">
-        <Link className="bg-none border-none text-lg" to='/'>Início</Link>
-        <Link className="bg-none border-none text-lg" to='/quemsomos'>Quem somos</Link>
-        <Link className="bg-none border-none text-lg" to='/faq'>Faq</Link>
-        <Link className="bg-none border-none text-lg" to='/suporte'>Suporte</Link>
+        <nav className="bottom-links">
+        <Link className="bottom-links-item" to='/'>
+            <img src={home} alt="Início" className="bottom-links-icon" />
+            Início
+        </Link>
+        <Link className="bottom-links-item" to='/quemsomos'>
+            <img src={quemSomos} alt="Quem somos" className="bottom-links-icon" />
+            Quem somos
+        </Link>
+        <Link className="bottom-links-item" to='/faq'>
+            <img src={faq} alt="FAQ" className="bottom-links-icon" />
+            FAQ
+        </Link>
+        <Link className="bottom-links-item" to='/suporte'>
+            <img src={chatbot} alt="Suporte" className="bottom-links-icon" />
+            Suporte
+        </Link>
         </nav>
     );
 };
