0a7151a4276915611b3faecd7723f2eebd6a0e55
PietroWilhelm <pietrowilhelm@git.com>
2025-09-24 13:58:28 -0300

    Escondendo o icone do Perfil nas paginas cadastro, login e loginComSenha

--- HealthAssistant/src/components/Header/header.tsx
+++ HealthAssistant/src/components/Header/header.tsx
@@ -1,18 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logoHc from '../../img/logo_hc.svg';
 import logoImrea from '../../img/logo_imrea.svg';
 import fotoPerfil from '../../img/perfil.png';
 
 export const Header = () => {
+  const location = useLocation();
+  
+  // Páginas onde o ícone do perfil deve ficar oculto
+  const paginasSemPerfil = ['/cadastro', '/login', '/logincomsenha'];
+  const ocultarPerfil = paginasSemPerfil.includes(location.pathname);
+
   return (
     <header className="header">
       <div className="header-logos">
         <img src={logoHc} alt="Logo HC" className="header-logo" />
         <img src={logoImrea} alt="Logo IMREA" className="header-logo" />
       </div>
-      <Link to="/perfil">
-        <img src={fotoPerfil} alt="Foto de Perfil" className="header-profile" />
-      </Link>
+      {!ocultarPerfil && (
+        <Link to="/perfil">
+          <img src={fotoPerfil} alt="Foto de Perfil" className="header-profile" />
+        </Link>
+      )}
     </header>
   );
 }
